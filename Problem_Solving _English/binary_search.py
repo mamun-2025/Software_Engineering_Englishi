c221f50e64ddb2c1c08b54d4b383b2292d9e51c6
Mamun Bepari <beparimamun708@gmail.com>
2026-08-03 04:30:38 +0600

    Add binary search

---
+++ Problem_Solving _English/binary_search.py
@@ -0,0 +1,29 @@
+
+
+
+"""
+
+"""
+
+arr = [10, 20, 30, 40, 50, 60, 70]
+
+def binary_search(arr, target):
+   left = 0
+   right = len(arr) - 1
+
+   while left <= right:
+      mid = (left + right) // 2
+
+      if arr[mid] == target:
+         return mid 
+
+      elif arr[mid] < target:
+         left = mid + 1
+
+      else: 
+         right = mid - 1 
+
+   return -1
+
+print(binary_search(arr, 30))
+print(binary_search(arr, 50))
